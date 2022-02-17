843f5be2072d193e998b7ed776e056fb7641d617
lsh <lshzy137@gmail.com>
2022-02-16 23:44:08 -0500

    updated

--- scripts/grader.py
+++ scripts/grader.py
@@ -13,14 +13,13 @@ labnames = ["lab0-pingpong",  "lab1-clientserver",  "lab2-primarybackup",  "lab3
 parser = argparse.ArgumentParser()
 parser.add_argument('labnum', metavar='labnum', type=int, nargs=1, help='test lab')
 parser.add_argument('inpath', metavar='inpath', type=str, nargs='?',
-                    default='cps512-spring22', help='path to the code directory')
+                    default='./cps512-spring22', help='path to the code directory')
 parser.add_argument('outpath', metavar='outpath', type=str, nargs='?',
                     default='./', help='path to the result directory')
 parser.add_argument('labpath', metavar='labpath', type=str, nargs='?',
-                    default='/root/dslabs/dslabs-master', help='path to the lab directory')
+                    default='/root/dslabs/dslabs', help='path to the lab directory')
 args = parser.parse_args()
 
-args.labname = labnames[args.labnum[0]]
 args.inpath = os.getcwd()+"/"+args.inpath
 args.outpath = os.getcwd()+"/"+args.outpath
 
@@ -28,7 +27,6 @@ args.outpath = os.getcwd()+"/"+args.outpath
 def readResults():
     results = {}
     res_file = "%s/results.csv" % args.outpath
-    print(res_file)
     if os.path.exists(res_file):
         with open(res_file, "r") as fin:
             reader = csv.reader(fin)
@@ -46,16 +44,17 @@ for repo in repos:
     if repo in results:
         continue
 
-    print("Grading %s...", repo)
+    print("Grading %s..." % repo)
     repopath = "%s/%s" % (args.inpath, repo)
     if not os.path.isdir(repopath):
         continue
     shutil.rmtree("%s/build" % args.labpath, ignore_errors=True)
 
-    src = "%s/labs/%s/src" % (repopath, args.labname)
-    dst = "%s/labs/%s/src" % (args.labpath, args.labname)
-    shutil.rmtree(dst, ignore_errors=True)
-    shutil.copytree(src, dst)
+    for labname in labnames:
+        src = "%s/labs/%s/src" % (repopath, labname)
+        dst = "%s/labs/%s/src" % (args.labpath, labname)
+        shutil.rmtree(dst, ignore_errors=True)
+        shutil.copytree(src, dst)
 
     with subprocess.Popen(("python3 run-tests.py --lab %d" % args.labnum[0]).split(), stdout=subprocess.PIPE, stderr=subprocess.DEVNULL) as proc:
         out, err = proc.communicate()
